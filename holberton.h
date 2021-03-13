3aecc8c45e6a014c88186d1e55220e8297c7e349
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-13 23:23:29 +0000

    Added terminator and updated libraries

--- holberton.h
+++ holberton.h
@@ -4,9 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * struct s_type- structure
+ * @args- pointer arguments
+ * @func: pointer function
+ */
+typedef struct s_type
+{
+	char *args;
+	int (*func)(va_list);
+} s_type;
 
 int (*_typefor(const char *argu, int argb))(va_list);
 int _putchar(char c);
+int (*func)(va_list);
 int _printf(const char *format, ...);
+int p_string(va_list valist);
+int p_int(va_list valist);
+int count_num(unsigned int n);
+int p_rot13(char *s);
+int print_R(va_list valist);
+int p_binary(va_list valist);
 
 #endif
