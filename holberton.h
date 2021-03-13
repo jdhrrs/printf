107b5f9581eb507ff2364491bbf5c11f60332932
Justin Harris <okstateboy2015@gmail.com>
2021-03-13 23:13:51 +0000

    Updated header with proto types

---
+++ holberton.h
@@ -0,0 +1,33 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include<unistd.h>
+
+/**
+ * struct s_type- structure 
+ * @args- pointer arguments 
+ * @func: pointer function 
+ */ 
+typedef struct s_type
+{
+  char *args;
+  int (*func)(va_list);
+} s_type;
+
+
+
+
+int _putchar(char c);
+int _printf(const char *format, ...)
+int (*_typefor(const char *argu, int argb))(va_list);
+int p_char(va_list valist);
+int p_string(va_list valist);
+int p_int(va_list valist);
+int count_num(unsigned int n);
+int p_rot13(char *s);
+int print_R(va_list valist);
+int p_binary(va_list valist)
+#endif
