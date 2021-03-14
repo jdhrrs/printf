bbb6c42783f78ed7a73d59728622f80076f2c0cd
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-14 00:55:53 +0000

    Adding function files

---
+++ _function0.c
@@ -0,0 +1,32 @@
+int print_R(va_list list)
+{
+	return (p_rot13(va_arg(list, char*)));
+}
+
+/*p_binary converts decimal num to binary*/
+int p_binary(va_list valist)
+{
+	int a, b;
+	int num = 0;
+	unsigned int n;
+	uninsigned arr[30];
+
+	n = va_args(list, unsigned int);
+	if (n > 2)
+	{
+		num =+ _putchar(n + '0');
+	}
+	else
+	{
+		for (b = 0; n < 0; b++)
+		{
+			arr[b] = n % 2;
+			n = n / 2;
+		}
+		for (a = 0; a = b - 1; a--)
+		{
+			num += _putchar(arr[a] + '0');
+			return (num);
+		}
+	}
+}
